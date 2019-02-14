16f009d9936945a98759430bf21f33f10b4e5471
lordminx <admin@story-games.at>
2019-02-14 23:43:41 +0100

    Now actually does what it's supposed to do.

--- GIMP_expanded.py
+++ GIMP_expanded.py
@@ -1,21 +1,36 @@
 # coding: utf-8
-titel = "GIMP"
 
-expanded = titel + "\n\n" + "GNU Image Manipulation Program"
+def word_count(string):
+    """Roughly count words by splitting on spaces."""
+    return len(string.split(" "))
 
-temp = expanded + "\n\n"
+def expand_gnu(string):
+    """Expand the 'GNU' acronym."""
+    return string.replace("GNU", "GNU's Not UNIX")
 
-result = temp
+def expand_unix(string):
+    """Expand the UNIX 'acronym'."""
+    return string.replace("UNIX", "Uniplexed Information Computing System")
 
-while count_words(result) < 50000:
-    if "GNU" in temp:
-        temp = expand_gnu(temp)
-        result += temp
-    if "UNIX" in temp:
-        temp = expand_unix(temp)
-        result += temp
-        
-with open("GIMP_expanded.txt") as f:
-    f.write(result)
-    
-    
+
+if __name__ == "__main__":
+    title = "GIMP"
+
+    expanded = "GNU Image Manipulation Program"
+
+    temp = expanded + "\n\n"
+
+    result = title + "\n\n" + temp
+
+    while word_count(result) < 50000:
+        if "GNU" in temp:
+            temp = expand_gnu(temp)
+            result += temp
+        if "UNIX" in temp:
+            temp = expand_unix(temp)
+            result += temp
+
+    with open("GIMP_expanded.txt", "w") as f:
+        f.write(result)
+
+
